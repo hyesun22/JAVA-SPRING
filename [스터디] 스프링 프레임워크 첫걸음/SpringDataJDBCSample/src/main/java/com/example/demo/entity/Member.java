2ab4ea90e23c40982baf01cd675191fef2ea3da2
hyesun22 <110083948+hyesun22@users.noreply.github.com>
2022-10-23 01:35:27 +0900

    commit pg(+error)

--- [스터디] 스프링 프레임워크 첫걸음/SpringDataJDBCSample/src/main/java/com/example/demo/entity/Member.java
+++ [스터디] 스프링 프레임워크 첫걸음/SpringDataJDBCSample/src/main/java/com/example/demo/entity/Member.java
@@ -1,4 +1,4 @@
-package com.demo.entity;
+package com.example.demo.entity;
 
 import org.springframework.data.annotation.Id;
 
@@ -6,21 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * 
- * Member 테이블 : 엔티티
- *
- */
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
-
-	public Member(Integer i, String string) {
-		// TODO Auto-generated constructor stub
-	}
-	private Integer id;
-	private String name;
 	
+	public Member(Object object, String string) {
+	}
+	
+	@Id
+	private Integer id;
+	private String  name;
 }
