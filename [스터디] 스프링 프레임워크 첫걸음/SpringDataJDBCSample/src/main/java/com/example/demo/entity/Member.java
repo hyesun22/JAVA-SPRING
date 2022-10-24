86480ed82c62e4269d2defe82ff32af6ba770c80
hyesun22 <110083948+hyesun22@users.noreply.github.com>
2022-10-24 14:26:41 +0900

    Update Member.java

--- [스터디] 스프링 프레임워크 첫걸음/SpringDataJDBCSample/src/main/java/com/example/demo/entity/Member.java
+++ [스터디] 스프링 프레임워크 첫걸음/SpringDataJDBCSample/src/main/java/com/example/demo/entity/Member.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
 
-import org.springframework.data.annotation.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Member {
 
-	
-	@Id
 	private Integer id;
 	private String  name;
 }
